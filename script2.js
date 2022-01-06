84d228f9b674f002c9c2ce1b0e525b6b67d30398
BhanuSaiTejaMarisa <bhanu3475@gmail.com>
2022-01-06 12:04:30 +0530

    added min num

--- script2.js
+++ script2.js
@@ -1,13 +1,14 @@
-var thirdPage=document.getElementById("thirdPage");
-thirdPage.addEventListener("click",()=>{
-    console.log("red");
-    window.location.href="./graphs.html";//"./data%20analysis.html"
-})
+// var thirdPage=document.getElementById("thirdPage");
+// thirdPage.addEventListener("click",()=>{
+//     console.log("red");
+//     window.location.href="./graphs.html";//"./data%20analysis.html"
+// })
 
 
 
-var prevPage=document.getElementById("prevPage");
-prevPage.addEventListener("click",()=>{
-    console.log("red");
-    window.location.href="./index.html";//"./data%20analysis.html"
-})
+
+// var prevPage=document.getElementById("prevPage");
+// prevPage.addEventListener("click",()=>{
+//     console.log("red");
+//     window.location.href="./index.html";//"./data%20analysis.html"
+// })
--- script3.js
+++ script3.js
@@ -1,5 +1,10 @@
-var prevPage=document.getElementById("prevPage");
-prevPage.addEventListener("click",()=>{
-    console.log("red");
-    window.location.href="./data%20analysis.html"
-})
+// var prevPage=document.getElementById("prevPage");
+// prevPage.addEventListener("click",()=>{
+//     console.log("red");
+//     window.location.href="./data%20analysis.html"
+// })
+
+// var fourth=document.querySelectorAll('input[type="number"]');
+// fourth[0].value=2;
+// fourth[1].value=2;
+// console.log(fourth[0].value,fourth[1].value);
